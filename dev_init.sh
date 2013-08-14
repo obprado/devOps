592ff1a1dc7160d781f5ba9b4ceb8b79772db07c
root <root@d3223a5d-6413-43ec-af0b-0128e60e9857.(none)>
2013-08-14 11:10:28 +0000

    Another try to set up the script. Hope this one works

---
+++ dev_init.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y emacs
+sudo apt-get install -y curl
+sudo apt-get install -y git-core
+\curl -L http://get.rvm.io | bash -s stable --ruby
+rvm requirements
+sudo apt-get install -y ruby irb rdoc
+gem install rails
