1adb9e648678570eb3101277e2d3f79d25ee8bbe
root <root@4ca8e8bd-9321-4149-814a-e7c718b67a20.(none)>
2013-08-19 12:05:10 +0000

    Added installation of rails debugger

--- dev_init.sh
+++ dev_init.sh
@@ -14,3 +14,11 @@ heroku keys:add
 git config --global core.editor emacs
 sudo apt-get install libpq-dev
 bundle install
+mkdir /usr/local/var
+mkdir /usr/local/var/postgres
+chmod 775 /usr/local/var/postgres/
+chown root /usr/local/var/postgres/
+wget https://raw.github.com/nex3/haml-mode/master/haml-mode.el ~/.emacs.d/
+echo "(add-to-list 'load-path "~/.emacs.d/")" >> ~/.emacs.d/init.el
+echo "(load "haml-mode")" >> ~/.emacs.d/init.el
+gem install debugger
