684958990131f4254c850380c67e16f2b38391c6
obprado <obprado@gmail.com>
2013-12-02 10:32:43 -0800

    Agregado mercurial a la lista de paquetes a instalar.

--- dev_init.sh
+++ dev_init.sh
@@ -3,6 +3,7 @@ sudo apt-get upgrade
 sudo apt-get install -y emacs
 sudo apt-get install -y curl
 sudo apt-get install -y git-core
+sudo apt-get install -y mercurial
 \curl -L http://get.rvm.io | bash -s stable --ruby
 rvm requirements
 sudo apt-get install -y ruby irb rdoc
